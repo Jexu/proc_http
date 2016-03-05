04279270b20e51965d81490afe74d51c07b089fb
zhengguo xu <ttjekux@163.com>
2016-03-05 16:05:01 +0800

    functions related to parsing request line and request header finished

--- include/_httphander.h
+++ include/_httphander.h
@@ -2,48 +2,27 @@
 #define HTTP_HANDER_H
 #include <iostream>
 #include <vector>
+#include <map>
 #include <string>
+#include <string.h>
+#include <stdlib.h>
 #include <exception>
 
-#include <string>
-#include <vector>
+#include <errno.h>
 #include <sys/socket.h>
 #include "_threadpool.h"
 #include "_epollutil.h"
 
 /*This class is used to combine hander with some process functions. When the
-  *event listened happens, new instatnce of this class will be created and added
-  *to the task queue, waitting to be handled by the idle thread. As definition
-  *in the class theadpool, scheduler thread will call process() function to complete
-  *its task.*/
+ *event listened happens, new instatnce of this class will be created and added
+ *to the task queue, waitting to be handled by the idle thread. As definition
+ *in the class theadpool, scheduler thread will call process() function to complete
+ *its task.*/
 class http_hander : public _hander
 {
 	public:
-		/*constructor*/
-		http_hander();
-		virtual ~http_hander();
-	public:
-		/*using this function to init the instance of this class. Arg 
-		  *socketfd is the value that accept() returned.*/
-		void init(int socketfd,const struct sockaddr_in& addr);
-		/*close the tcp connection*/
-		void close(bool is_close = true);
-		/*This is the function defined in the abstract class _hander and 
-		  *it is the entrypint function of HTTP request*/
-		virtual void* process();
-		/*read the whole text of HTTP request message into the read buffer*/
-		bool read();
-		/*write response datas into the write buffer*/
-		bool write();
-	public:
-		/*all socketfd will be registered to the same epoll kernel event 
-		  *table. so epollfd is static*/
-		static int epollfd;
-		/*count amount of users*/
-		static int user_counter;
-	public:
 		/*record the status that which part of HTTP request message is 
-		  *being parsed*/
+		 *being parsed*/
 		enum CHECK_STATUS {CHECK_STATUS_REQUESTLINE, CHECK_STATUS_HEADER,
 			CHECK_STATUS_CONTENT};
 		/*result code of parsing*/
@@ -53,23 +32,63 @@ class http_hander : public _hander
 		/*methods of HTTP request*/
 		enum METHODS {GET, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT,
 			PATCH};
+		enum READ_INTO_BUF_STATUS {BUF_FULL, CLIENT_CLOSED, FAIL_OTHER, 
+			READ_SUCCESS};
+		enum LINE_STATUS {LINE_OK = 0, LINE_BAD, LINE_OPEN};
+
+		enum REQUEST_HEADERS {ACCEPT, ACCEPT_CHARSET, ACCEPT_ENCODING, 
+			ACCEPT_LANGUAGE, ACCEPT_RANGE, AUTHORIZTION, CACHE_CONTROL, 
+			CONNECTION, COOKIE, CONTENT_LENGTH, CONTENT_TYPE, DATE, 
+			EXPECT, FROM, HOST, IF_MATCH, IF_MODIFIED_SINCE, IF_NONE_MATCH, 
+			IF_RANGE, IF_UNMODIFIED_SINCE, MAX_FORWARDS, PRAGMA, 
+			PROXY_AUTHORIZATION, RANGE, TE, UPGRADE, USER_AGENT, VIA, WARING};
+
+	public:
+		/*constructor*/
+		http_hander();
+		virtual ~http_hander();
+	public:
+		/*using this function to init the instance of this class. Arg 
+		 *socketfd is the value that accept() returned.*/
+		void init(int socketfd,const struct sockaddr_in& addr);
+		/*close the tcp connection*/
+		void close(bool is_close = true);
+		/*This is the function defined in the abstract class _hander and 
+		 *it is the entrypint function of HTTP request*/
+		virtual void* process();
+		/*read the whole text of HTTP request message into the read buffer*/
+		READ_INTO_BUF_STATUS read();
+		/*write response datas into the write buffer*/
+		bool write();
+	public:
+		/*all socketfd will be registered to the same epoll kernel event 
+		 *table. so epollfd is static*/
+		static int epollfd;
+		/*count amount of users*/
+		static int user_counter;
+		static const int READ_BUFFER_SIZE = 2048;
 	private:
 		/*init some inner variables*/
 		void init();
 		/*entrypoint function of parsing HTTP request message*/
 		HTTP_CODE process_request();
-		/*following three functions will be called by process_request() to parse 
-		  *request line, request header and request content respectively.*/
+		/*following four functions will be called by process_request() to parse 
+		 *request line, request header, request content and get line from read buffer
+		 *respectively.*/
 		HTTP_CODE parse_request_line(char* text);
+		bool parse_request_method(char* method);
 		HTTP_CODE parse_request_header(char* text);
 		HTTP_CODE parse_request_content(char* text);
+		LINE_STATUS parse_line();
+		char* get_line();
+		HTTP_CODE do_request();
 
 		/*entrypoint function of sending response to client.*/
 		bool process_response();
 
 
 		/*function _split is applied to split string and result will be stored 
-		  *into arg splits*/
+		 *into arg splits*/
 		void _split(const std::string *src,const char* sp, 
 				std::vector<std::string>* splits)const;
 		void _split(const char *src,const char* sp,
@@ -82,5 +101,18 @@ class http_hander : public _hander
 		const struct sockaddr_in* addr;
 		/*it is a util*/
 		epoll_util eputil;
+	private:
+		int idel_r_buffer_idx;
+		int cur_start_line_idx;
+		int checked_line_idx;
+		CHECK_STATUS checked_status;
+
+		METHODS request_method;
+		char* request_url;
+		char* http_version;
+
+		std::map<REQUEST_HEADERS, char*> map_request_headers;
+		
+		char read_buffer[READ_BUFFER_SIZE];
 };
 #endif
--- src/_httphander.cpp
+++ src/_httphander.cpp
@@ -30,9 +30,31 @@ void* http_hander::process()
 	return NULL;
 }
 
-bool http_hander::read()
+http_hander::READ_INTO_BUF_STATUS http_hander::read()
 {
-	return true;
+	if(idel_r_buffer_idx >= READ_BUFFER_SIZE)
+	{
+		return BUF_FULL;
+	}
+	int read_bytes = 0;
+	for(;;)
+	{
+		read_bytes = recv(this->socketfd, read_buffer + idel_r_buffer_idx, 
+				READ_BUFFER_SIZE - idel_r_buffer_idx, 0);
+		if(read_bytes == -1)
+		{
+			if(errno == EWOULDBLOCK || errno == EAGAIN)
+			{
+				break;
+			}
+			return FAIL_OTHER;
+		}
+		else if(read_bytes == 0)
+		{
+			return CLIENT_CLOSED;
+		}
+	}
+	return READ_SUCCESS;
 }
 
 bool http_hander::write()
@@ -84,27 +106,356 @@ void http_hander::_split(const char *src,const char* sp,
 
 void http_hander::init()
 {
+	this->idel_r_buffer_idx = 0;
+	this->cur_start_line_idx = 0;
+	this->checked_line_idx = 0;
+	this->checked_status = CHECK_STATUS_REQUESTLINE;
+	request_method = GET;
+	request_url = NULL;
+	http_version = NULL;
+	map_request_headers.clear();
+	memset(read_buffer,'\0',READ_BUFFER_SIZE);
 }
 
 http_hander::HTTP_CODE http_hander::process_request()
 {
-	return FORBIDDEN_REQUEST;
+	LINE_STATUS line_status = LINE_OK;
+	HTTP_CODE ret_code = NO_REQUEST;
+	char* text = NULL;
+	while(((checked_status == CHECK_STATUS_CONTENT) && (line_status == LINE_OK)) 
+			|| ((line_status = parse_line()) == LINE_OK))
+	{
+		/*因为没一行的\r\n都已经被\0填上了，所以可以得到一行*/
+		text = get_line();
+		cur_start_line_idx = checked_line_idx;
+
+		switch(checked_status)
+		{
+			case CHECK_STATUS_REQUESTLINE:
+			{
+				ret_code = parse_request_line(text);
+				if(ret_code == BAD_REQUEST)
+				{
+					return BAD_REQUEST;
+				}
+				break;
+			}
+			case CHECK_STATUS_HEADER:
+			{
+				ret_code = parse_request_header(text);
+				if(ret_code == BAD_REQUEST)
+				{
+					return BAD_REQUEST;
+				}
+				else if(ret_code == GET_REQUEST)
+				{
+					return do_request();
+				}
+				break;
+			}
+			case CHECK_STATUS_CONTENT:
+			{
+				ret_code = parse_request_content(text);
+				if(ret_code == GET_REQUEST)
+				{
+					return do_request();
+				}
+				line_status = LINE_OPEN;
+				break;
+			}
+			default:
+			{
+				return INTERNAL_ERROR;
+				break;
+			}
+		}
+	}
+	return NO_REQUEST;
 }
 
 
 http_hander::HTTP_CODE http_hander::parse_request_line(char* text)
 {
-	return FORBIDDEN_REQUEST;
+	this->request_url = strpbrk(text, "\t");
+	if(!this->request_url)
+	{
+		return BAD_REQUEST;
+	}
+	*this->request_url++ = '\0';
+	char* method = text;
+	if(!parse_request_method(method))
+	{
+		return BAD_REQUEST;
+	}
+
+	this->request_url += strspn(request_url, "\t");
+
+	this->http_version = strpbrk(this->request_url, "\t");
+	if(!this->http_version)
+	{
+		return BAD_REQUEST;
+	}
+	*this->http_version++ = '\0';
+	this->http_version += strspn(this->http_version, "\t");
+	if(strcasecmp(this->http_version, "HTTP/1.1") != 0)
+	{
+		return BAD_REQUEST;
+	}
+	
+	if(strncasecmp(this->request_url, "http://", 7) == 0)
+	{
+		this->request_url = strchr(this->request_url, '/');
+	}
+	if(!this->request_url || this->request_url[0] != '/')
+	{
+		return BAD_REQUEST;
+	}
+
+	checked_status = CHECK_STATUS_HEADER;
+
+	return NO_REQUEST;
+}
+
+
+bool http_hander::parse_request_method(char* method)
+{
+	if(strcasecmp(method, "GET") == 0)
+	{
+		this->request_method = GET;
+	}
+	else if(strcasecmp(method, "POST") == 0)
+	{
+		this->request_method = POST;
+	}
+	else if(strcasecmp(method, "HEAD") == 0)
+	{
+		this->request_method = HEAD;
+	}
+	else if(strcasecmp(method, "PUT") == 0)
+	{
+		this->request_method = PUT;
+	}
+	else if(strcasecmp(method, "DELETE") == 0)
+	{
+		this->request_method = DELETE;
+	}
+	else if(strcasecmp(method, "TRACE") == 0)
+	{
+		this->request_method = TRACE;
+	}
+	else if(strcasecmp(method, "OPTIONS") == 0)
+	{
+		this->request_method = OPTIONS;
+	}
+	else if(strcasecmp(method, "CONNECT") == 0)
+	{
+		this->request_method = CONNECT;
+	}
+	else if(strcasecmp(method, "PATCH") == 0)
+	{
+		this->request_method = PATCH;
+	}
+	else 
+	{
+		return false;
+	}
+	return true;
 }
 
 http_hander::HTTP_CODE http_hander::parse_request_header(char* text)
 {
-	return FORBIDDEN_REQUEST;
+	if(text[0] == '\0')
+	{
+		//finishing parsing request headers and coming cross to blank line
+		if(atoi(map_request_headers[CONTENT_LENGTH]) != 0)
+		{
+			//http request message contains content
+			checked_status = CHECK_STATUS_CONTENT;
+			return NO_REQUEST;
+		}
+	}
+	else if(strncasecmp(text, "accept:", 7) == 0)
+	{
+		text += 7;
+		text += strspn(text, "\t");
+		map_request_headers[ACCEPT] = text;
+	}
+	else if(strncasecmp(text, "accept-charset:", 15) == 0)
+	{
+		text += 15;
+		text += strspn(text, "\t");
+		map_request_headers[ACCEPT_CHARSET] = text;
+	}
+	else if(strncasecmp(text, "accept-encoding:", 16) == 0)
+	{
+		text += 16;
+		text += strspn(text, "\t");
+		map_request_headers[ACCEPT_ENCODING] = text;
+	}
+	else if(strncasecmp(text, "accept-language:", 16) == 0)
+	{
+		text += 16;
+		text += strspn(text, "\t");
+		map_request_headers[ACCEPT_LANGUAGE] = text;
+	}
+	else if(strncasecmp(text, "accept-range:", 13) == 0)
+	{
+		text += 13;
+		text += strspn(text, "\t");
+		map_request_headers[ACCEPT_RANGE] = text;
+	}
+	else if(strncasecmp(text, "authorization:", 15) == 0)
+	{
+		text += 15;
+		text += strspn(text, "\t");
+		map_request_headers[AUTHORIZTION] = text;
+	}
+	else if(strncasecmp(text, "cache-control:", 14) == 0)
+	{
+		text += 14;
+		text += strspn(text, "\t");
+		map_request_headers[CACHE_CONTROL] = text;
+	}
+	else if(strncasecmp(text, "connection:", 11) == 0)
+	{
+		text += 11;
+		text += strspn(text, "\t");
+		map_request_headers[CONNECTION] = text;
+	}
+	else if(strncasecmp(text, "cookie:", 7) == 0)
+	{
+		text += 7;
+		text += strspn(text, "\t");
+		map_request_headers[COOKIE] = text;
+	}
+	else if(strncasecmp(text, "content-length:", 16) == 0)
+	{
+		text += 16;
+		text += strspn(text, "\t");
+		map_request_headers[CONTENT_LENGTH] = text;
+	}
+	else if(strncasecmp(text, "content-type:", 13) == 0)
+	{
+		text += 13;
+		text += strspn(text, "\t");
+		map_request_headers[CONTENT_TYPE] = text;
+	}
+	else if(strncasecmp(text, "date:", 5) == 0)
+	{
+		text += 5;
+		text += strspn(text, "\t");
+		map_request_headers[DATE] = text;
+	}
+	else if(strncasecmp(text, "expect:", 7) == 0)
+	{
+		text += 7;
+		text += strspn(text, "\t");
+		map_request_headers[EXPECT] = text;
+	}
+	else if(strncasecmp(text, "from:", 5) == 0)
+	{
+		text += 5;
+		text += strspn(text, "\t");
+		map_request_headers[FROM] = text;
+	}
+	else if(strncasecmp(text, "host:", 5) == 0)
+	{
+		text +=	5; 
+		text += strspn(text, "\t");
+		map_request_headers[HOST] = text;
+	}
+	else if(strncasecmp(text, "if-match:", 9) == 0)
+	{
+		text += 9; 
+		text += strspn(text, "\t");
+		map_request_headers[IF_MATCH] = text;
+	}
+	else if(strncasecmp(text, "if-modified-since:", 18) == 0)
+	{
+		text += 18;
+		text += strspn(text, "\t");
+		map_request_headers[IF_MODIFIED_SINCE] = text;
+	}
+	else if(strncasecmp(text, "if-none-match:", 14) == 0)
+	{
+		text += 14; 
+		text += strspn(text, "\t");
+		map_request_headers[IF_NONE_MATCH] = text;
+	}
+	else if(strncasecmp(text, "if-range:", 9) == 0)
+	{
+		text += 9; 
+		text += strspn(text, "\t");
+		map_request_headers[IF_RANGE] = text;
+	}
+	else if(strncasecmp(text, "if-unmodified-since:", 20) == 0)
+	{
+		text += 20;
+		text += strspn(text, "\t");
+		map_request_headers[IF_UNMODIFIED_SINCE] = text;
+	}
+	else if(strncasecmp(text, "max-forwards:", 13) == 0)
+	{
+		text += 13;
+		text += strspn(text, "\t");
+		map_request_headers[MAX_FORWARDS] = text;
+	}
+	else if(strncasecmp(text, "pragma:", 7) == 0)
+	{
+		text += 7;
+		text += strspn(text, "\t");
+		map_request_headers[PRAGMA] = text;
+	}
+	return NO_REQUEST;
 }
 
 http_hander::HTTP_CODE http_hander::parse_request_content(char* text)
 {
 	return FORBIDDEN_REQUEST;
+}
+
+http_hander::LINE_STATUS http_hander::parse_line()
+{
+	char temp;
+	for(;checked_line_idx < idel_r_buffer_idx; ++checked_line_idx)
+	{
+		temp = read_buffer[checked_line_idx];
+		if(temp == '\r')
+		{
+			if(read_buffer[checked_line_idx + 1] == '\n')
+			{
+				read_buffer[checked_line_idx++] = '\0';
+				read_buffer[checked_line_idx++] = '\0';
+				return LINE_OK;
+			}
+			else if(checked_line_idx + 1 == idel_r_buffer_idx)
+			{
+				return LINE_OPEN;
+			}
+			return LINE_BAD;
+		}
+		else if(temp == '\n')
+		{
+			if(checked_line_idx > 1 && read_buffer[checked_line_idx -1] == '\r')
+			{
+				read_buffer[checked_line_idx -1] = '\0';
+				read_buffer[checked_line_idx++] = '\0';
+				return LINE_OK;
+			}
+			return LINE_BAD;
+		}
+	}
+	return LINE_OPEN;
+}
+
+char* http_hander::get_line()
+{
+	return read_buffer + cur_start_line_idx;
+}
+
+http_hander::HTTP_CODE http_hander::do_request()
+{
+	return NO_REQUEST;
 }
 
 bool http_hander::process_response()
